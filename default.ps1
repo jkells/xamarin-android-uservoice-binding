304c478e5aee2f7f602d3602fbec81187dcdaf4a
Jared Kells <jkells@gmail.com>
2015-01-21 17:14:37 +1100

    Latest build tools, make it work again :(

--- default.ps1
+++ default.ps1
@@ -1,7 +1,6 @@
 properties { 
   $base_dir = resolve-path .  
-  $nuget_bin = "$base_dir\tools\.nuget\nuget.exe"
-  $7zip_bin = "$base_dir\tools\7zip\7za.exe"
+  $nuget_bin = "$base_dir\.nuget\nuget.exe"
   $project_dir = "$base_dir\UserVoice.Binding"  
   $sln_file = "$base_dir\UserVoice.Binding.sln"  
   $jar_dir = "$project_dir\jars"
@@ -9,25 +8,21 @@ properties {
 
   $android_sdk_dir = $env:ANDROID_SDK_HOME
   $android_library_dir = "$base_dir\uservoice-android-sdk"
-  $android_library_build_dir = "$android_library_dir\UserVoiceSDK\build\bundles\release"
+  $android_library_build_dir = "$android_library_dir\UserVoiceSDK\build\outputs\aar"
 }
 
 Framework "4.0"
 
-task default -depends Package
+task default -depends Clean,Package
 
 task Clean {  
   remove-item -force -recurse $project_dir\obj -ErrorAction SilentlyContinue
-  remove-item -force -recurse $project_dir\bin -ErrorAction SilentlyContinue
-  remove-item -force -recurse "$jar_dir\bin" -ErrorAction SilentlyContinue
-  remove-item -force -recurse "$jar_dir\res" -ErrorAction SilentlyContinue
-  remove-item -force -recurse "$jar_dir\assets" -ErrorAction SilentlyContinue
-  remove-item -force "$jar_dir\*.zip" -ErrorAction SilentlyContinue
-  remove-item -force "$jar_dir\*.jar" -ErrorAction SilentlyContinue  
-  remove-item -force "$android_library_dir\UserVoiceSDK\build" -ErrorAction SilentlyContinue  
+  remove-item -force -recurse $project_dir\bin -ErrorAction SilentlyContinue  
+  remove-item -force "$jar_dir\*.aar" -ErrorAction SilentlyContinue
+  remove-item -force -recurse "$android_library_dir\UserVoiceSDK\build" -ErrorAction SilentlyContinue  
 }
 
-task Compile -depends Clean,Copy-Jars {
+task Compile -depends Copy-Jars {
     msbuild $sln_file /p:"Configuration=$config"
 }
 
@@ -37,19 +32,8 @@ task Package -depends Compile{
     }
 }
 
-task Copy-Jars -depends Clean,Test-Environment{
-    Copy-Item -Force "$android_library_build_dir\*.jar" "$jar_dir"
-    Copy-Item -Recurse -Force "$android_library_build_dir\res" "$jar_dir"
-    Copy-Item -Recurse -Force "$android_library_build_dir\assets" "$jar_dir"
-        
-    # Compress the binaries and resources into the library package zip
-    exec{
-        & $7zip_bin a -r "$jar_dir\library.zip" "$jar_dir\bin\"
-        & $7zip_bin a -r "$jar_dir\library.zip" "$jar_dir\res\"
-    }
-
-    Remove-Item -recurse -force "$jar_dir\res"
-    Remove-Item -recurse -force "$jar_dir\assets"
+task Copy-Jars -depends Build-Java-Library{
+    Copy-Item -Force "$android_library_build_dir\*.aar" "$jar_dir"
 }
 
 task Test-Environment{
@@ -63,14 +47,14 @@ task Test-Environment{
 }
 
 task Build-Java-Library -depends Test-Environment{
-    & "$env:ANDROID_SDK_HOME\tools\android" update lib-project --path "$android_library_dir\UserVoiceSDK"
-    & "$env:ANDROID_SDK_HOME\tools\android" update project --path "$android_library_dir\UVDemo"
+    & "$env:ANDROID_SDK_HOME\tools\android" update lib-project --path "$android_library_dir\UserVoiceSDK" --target android-21
+    & "$env:ANDROID_SDK_HOME\tools\android" update project --path "$android_library_dir\UVDemo" --target android-21
      
-    cp -Force $android_library_dir\UserVoiceSDK\local.properties $android_library_dir
-    cp -Force $base_dir\nolint-build.gradle $android_library_dir\UserVoiceSDK\build.gradle
+    #cp -Force $android_library_dir\UserVoiceSDK\local.properties $android_library_dir
+    #cp -Force $base_dir\nolint-build.gradle $android_library_dir\UserVoiceSDK\build.gradle
 
     pushd    
     cd "$android_library_dir\UserVoiceSDK"
-    .\gradlew.bat build
+    .\gradlew.bat assembleRelease
     popd
 }
